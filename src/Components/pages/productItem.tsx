4ebbb633b38fd519b9600b380a606d1c8ec2704f
mrbushik <mrbushik1@gmail.com>
2023-03-10 20:58:01 +0100

    get product name from url

--- src/Components/pages/productItem.tsx
+++ src/Components/pages/productItem.tsx
@@ -1,6 +1,22 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { ourProducts } from "../products";
+import { productItem } from "../intefaces";
 const ProductItem: React.FC = () => {
+  const location: any = useLocation();
+
+  const [targetProduct, setTargetProduct] = useState<productItem | null>();
+
+  const actualityProductName: string = location.pathname.slice(1);
+
+  useEffect(() => {
+    const targetProduct = ourProducts.filter(
+      (item: productItem) => item.title === actualityProductName
+    );
+
+    // handleChangeCurrentTest();
+  }, []);
+
   return <div></div>;
 };
 
--- src/Components/ui/mainProductItem.tsx
+++ src/Components/ui/mainProductItem.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { productItem } from "../intefaces";
+import { Link } from "react-router-dom";
 
 const MainProductItem: React.FC<productItem> = ({ img, price, title }) => {
   return (
-    <div className='product__item-wrapper'>
+    <Link to={`products/${title}`} className="product__item-wrapper">
       <img src={img} alt={title} className="product__item-img" />
       <h4 className="product__item-title">{title}</h4>
       <p className="product__item-price">${price} USD</p>
-    </div>
+    </Link>
   );
 };
 
