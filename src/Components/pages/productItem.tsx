873b1bdfb85c16aa4554a367441d23b08afdef6c
mrbushik <mrbushik1@gmail.com>
2023-03-18 16:57:30 +0100

    styling product page

--- src/Components/pages/productItem.tsx
+++ src/Components/pages/productItem.tsx
@@ -26,10 +26,10 @@ const ProductItem: React.FC = () => {
               <img src={targetProduct.img} className="product__page-imgMain" />
             </div>
             <div>
-              <h1>{targetProduct.title}</h1>
-              <p>${targetProduct.price} USD</p>
+              <h1 className="produdct__page-title">{targetProduct.title}</h1>
+              <p className="product__page-price">${targetProduct.price} USD</p>
               {targetProduct.sizes && (
-                <div>
+                <div className="product__page-sizes">
                   <p>Size</p>
                   <ul>
                     {targetProduct.sizes.map((size: string, index: number) => (
@@ -39,16 +39,19 @@ const ProductItem: React.FC = () => {
                 </div>
               )}
               <p>Quantity</p>
-              <div>
-                <span>-</span>
-                <span></span>
-                <span>+</span>
+              <div className="product__page-quantity">
+                <span className="product__quantity-btn">-</span>
+                <span className="product__quantity-value">12</span>
+                <span className="product__quantity-btn">+</span>
               </div>
-              <div>Add to cart</div>
-              <div>Buy it now</div>
+              <div className="product__page-btns">
+              <div className="product__btn-card">Add to cart</div>
+              <div className="product__btn-now">Buy it now</div>
+              </div>
               <div>
-                {targetProduct.listInfo?.map((item: string, index: number) => (
-                  <div key={index}>
+
+                {targetProduct.titles?.map((item: string, index: number) => (
+                  <div key={index} className='product__info-block'>
                     <h5>{item}</h5>
                     <p>
                       {targetProduct?.descriptions &&
@@ -58,7 +61,7 @@ const ProductItem: React.FC = () => {
                 ))}
               </div>
               {targetProduct.listName && (
-                <div>
+                <div className="product__additional-wrapper">
                   <h5>{targetProduct.listName}</h5>
                   <ul>
                     {targetProduct.listInfo?.map(
@@ -73,8 +76,9 @@ const ProductItem: React.FC = () => {
           </div>
         )}
       </div>
-      <div>
-        <div>
+    <div className="container">
+    <div className="product__page-other-wrapper">
+        <div className="product__other-item">
           <h5>Free Shipping</h5>
           <p>
             Pair text with an image to focus on your chosen product, collection,
@@ -82,7 +86,7 @@ const ProductItem: React.FC = () => {
             review.
           </p>
         </div>
-        <div>
+        <div className="product__other-item">
           <h5>Hassle-Free Exchanges</h5>
           <p>
             Pair text with an image to focus on your chosen product, collection,
@@ -91,6 +95,7 @@ const ProductItem: React.FC = () => {
           </p>
         </div>
       </div>
+    </div>
     </div>
   );
 };
